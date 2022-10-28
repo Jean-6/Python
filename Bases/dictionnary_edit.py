5cb4e9f304c05b30145b57a2b1ba08ed37d41c1e
Jean <85187229+Jean-6@users.noreply.github.com>
2022-10-28 21:06:34 +0200

    add subject

--- Bases/dictionnary_edit.py
+++ Bases/dictionnary_edit.py
@@ -1,4 +1,15 @@
+"""
+Python - Dictionnaires : modifications
+Vous avez à votre disposition un dictionnaire (tableau associatif) nommé my_dict. 
 
+Dans l'ordre : 
+
+ajoutez une clé "hello", qui vaut "world"
+modifiez la clé "nombre", pour lui donner la valeur "0"
+supprimez la clé "pi" 
+
+
+"""
 
 def main() :
     my_dict["hello"]="world"		 	 	  	 	  		 		  
