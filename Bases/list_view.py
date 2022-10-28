6a5542b7168b0c74f560d214ccc8305e133085ed
Jean <85187229+Jean-6@users.noreply.github.com>
2022-10-28 20:53:25 +0200

    solved

---
+++ Bases/list_view.py
@@ -0,0 +1,8 @@
+def main() :
+		 	 	  	 	  		 		   	 	  	 	  		 		  
+    for i,v in enumerate(my_list):		 	 	  	 	  		 		  
+        print("{} : {}".format(i,v))		 	 	  	 	  		 		  
+		 	 	  	 	  		 		  
+
+if __name__ == "__main__":
+    main()
